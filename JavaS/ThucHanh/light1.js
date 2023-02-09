64367f2e1f579d00537db54f2b9797e80bf9fdaf
ThienJ97 <tieuquyy1002@gmail.com>
2023-02-09 21:54:01 +0700

    add PP2_1.html file

---
+++ JavaS/ThucHanh/light1.js
@@ -0,0 +1,12 @@
+function down() {
+    document.getElementById('car').src='down.jpg';
+}
+function left(){
+    document.getElementById('car').src='left.jpg';
+}
+function right() {
+    document.getElementById('car').src='right.jpg';
+}
+function up(){
+    document.getElementById('car').src='up.jpg';
+}
